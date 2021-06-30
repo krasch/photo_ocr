b82f119b670aad6b1fe5d502aa6489810f743c26
kat <krasch>
2021-06-30 14:02:12 +0200

    Use PIL for image resizing in transform

--- photo_ocr/detection/craft/preprocessing.py
+++ photo_ocr/detection/craft/preprocessing.py
@@ -11,13 +11,8 @@ class ResizeRatio:
         self.ratio = ratio
 
     def __call__(self, img: Image.Image):
-        # todo use PIL
-        #target_size = int(img.height * self.ratio), int(img.width * self.ratio)
-        #return F.resize(img, target_size, interpolation=Image.BILINEAR)
-
-        target_h, target_w = int(img.height * self.ratio), int(img.width * self.ratio)
-        resized = cv2.resize(np.array(img), (target_w, target_h), interpolation=cv2.INTER_LINEAR)
-        return Image.fromarray(resized)
+        target_size = int(img.height * self.ratio), int(img.width * self.ratio)
+        return F.resize(img, target_size, interpolation=Image.BILINEAR)
 
 
 class PadTo32:
